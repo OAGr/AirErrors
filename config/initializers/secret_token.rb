38122a5586081a334b42a909c190fceb99ad9feb
Ozzie Gooen <Ozzie_Gooen@hmc.edu>
2011-10-06 01:16:13 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Air::Application.config.secret_token = '48d3c288e8a0298e081734eebed7aa78f15fa491f7479638506a0a4d97342b979f9b859ba5b4e934929c554d8d1277a42345755eed414c2c6f7214af8c9a8c5a'
